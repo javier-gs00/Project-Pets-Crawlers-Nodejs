1838d9925b2ca1f783d3abca2422f208e94e4387
Javier Gutiérrez <28971285+javier-gs00@users.noreply.github.com>
2017-12-20 22:29:50 -0300

    Added function to create/update products

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const utils = require('../tools/utils')
 
-mongoose.Promise = global.Promise
+mongoose.Promise = require('bluebird')
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/projectpetsnodejs', {
     useMongoClient: true
 }, (err) => {
@@ -24,6 +24,42 @@ const ProductSchema = new mongoose.Schema({
 
 const ProductModel = mongoose.model('Product', ProductSchema)
 
+// Check for changes in documents. Takes an array of objects and returns an array of objects
+exports.updateBatch = data => {
+    return new Promise((resolve, reject) => {
+        // Declare an array for products to be updated
+        let toBeUpdated = []
+        data.forEach(product => {
+            ProductModel.findOne({
+                name: product.name, 
+                store: product.store, 
+                category: product.category, 
+                animal: product.animal}, (err, doc) => {
+                    // Throws an error if the product it's not found
+                    // In this case save the product
+                    if (err) {
+                        ProductModel.insert(product, res => {
+                            console.log('Saved a new product', res)
+                        })
+                    } else {
+                        // If no errors. Product was found. Check if price is different
+                        if (doc.price != product.price) {
+                            ProductModel.findByIdAndUpdate(doc.id, {
+                                price: product.price,
+                                date: new Date()
+                            }, (err, res) => {
+                                if (err) console.error(`Update to product id: ${doc.id} failed with err:`, err)
+                                console.log(`Update to product id: ${doc.id} succesfull`)
+                            })
+                        }
+                    }
+                })
+
+        })
+    })
+}
+
+// Makes a batch insert to the db. Takes an array of objects
 exports.writeBatch = data => {
     return new Promise((resolve, reject) => {
         utils.checkData(data)
@@ -52,7 +88,11 @@ exports.writeBatch = data => {
     })
 }
 
-// The following schema and functions are used to test features
+/* ================================================================== */
+// The following schema and functions are used to test changes and new features
+// on a similar dataset
+/* ================================================================== */
+
 const QuotesSchema = new mongoose.Schema({
     text: String,
     author: String,
@@ -77,6 +117,110 @@ exports.insertManyQuotes = data => {
                 if (err) console.error('Error disconnecting from MongoDB: ', err)
                 console.log('Disconnected from MongoDB')
             })
+            reject(err)
+        })
+    })
+}
+
+// Check for changes in documents. Takes an array of objects and returns an array of objects
+// exports.updateBatchQuotes = data => {
+//     return new Promise((resolve, reject) => {
+//         let counter = 0
+//         data.forEach(quote => {
+//             QuoteModel.findOne({text: quote.text}, (err, doc) => {
+//                     // Throws an error if the product it's not found
+//                     // In this case save the product
+//                     console.log('tic')
+//                     // console.log(typeof doc)
+//                     // console.log(doc)
+//                     // console.log(quote)
+//                     if (!doc) {
+//                         let newQuote = new QuoteModel(quote)
+//                         newQuote.save((err, newDoc) => {
+//                             console.log('Saved a new quote')                           
+//                             counter++
+//                         })
+//                         // QuoteModel.save(quote, res => {
+//                         //     console.log('Saved a new quote', res)
+//                         // })
+//                     } else {
+//                         // If no errors. Product was found. Check if price is different
+//                         if (doc.author != quote.author) {
+//                             QuoteModel.findByIdAndUpdate(doc.id, {
+//                                 text: quote.text,
+//                                 author: quote.author,
+//                                 date: quote.date
+//                             }, (err, res) => {
+//                                 if (err) console.error(`Update to quote id: ${doc.id} failed with err:`, err)
+//                                 console.log(`Update to quote id: ${doc.id} succesfull`)
+//                                 counter++
+//                             })
+//                         }
+//                     }
+//                 })
+//         })
+//         console.log(counter)
+//         if (counter == data.length) {
+//             db.close(err => {
+//                 if (err) console.error('Error disconnecting from MongoDB: ', err)
+//                 console.log('Disconnected from MongoDB')
+//             })
+//             resolve('Works!')
+//             reject(':(!!')
+//         }
+//     })
+// }
+
+// Check for changes in documents. Takes an array of objects and returns an array of objects
+exports.updateBatchQuotes = data => {
+    return new Promise((resolve, reject) => {
+        // let counter = 0
+        // data.forEach(quote => {
+        //     QuoteModel.findOne({text: quote.text})
+        //     .then(mongoQuote => {
+        //         console.log(mongoQuote)
+        //         resolve(mongoQuote)
+        //     })
+        //     .catch(err => {
+        //         reject(err)
+        //     })
+        // })
+        let promiseArray = []
+        // data.forEach(quote => {
+        //     QuoteModel.findOne({text: quote.text})
+        //     .then(doc => {
+        //         if(!doc) {
+        //             let newQuote = new QuoteModel(doc)
+        //             promiseArray.push(newQuote.save())
+        //         }
+        //         if(doc.author != quote.author) {
+        //             doc.author = quote.author
+        //             promiseArray.push(doc.save())
+        //         }
+        //     })
+        // })
+        for(let i = 0, n = data.length; i < n; i++) {
+            let query = {
+                text: data[i].text
+            }
+            let update = {
+                text: data[i].text,
+                author: data[i].author,
+                date: data[i].date
+            }
+            let options = {upsert: true, new: true, setDefaultsOnInsert: true}
+            promiseArray.push(QuoteModel.findOneAndUpdate(query, update, options))
+        }
+        Promise.all(promiseArray)
+        .then(results => {
+            resolve(results)
+            db.close(err => {
+                if (err) console.error('Error disconnecting from MongoDB: ', err)
+                console.log('Disconnected from MongoDB')
+            })
+        })
+        .catch(err => {
+            console.log(err)
             reject(err)
         })
     })
--- sequential/exe.js
+++ sequential/exe.js
@@ -5,26 +5,41 @@ const brainyquotes = require('../web_crawlers/_test_brainyquotes').brainyquotes
 const utils = require('../tools/utils')
 const db = require('../database/mongodb')
 
-executeMultipleCrawlers().then(data => {
-    let counter = 0
-    data.forEach(element => {
-        counter += 1
-    })
-    console.log(`${counter} products scraped`)
-    db.insertManyQuotes(data)
-    .then(docs => {
-        console.log(`Batch succesfull. ${docs.length} quotes saved to MongoDB`)
-    })
-    .catch(err => {
-        console.log('Error executing batch')
-    })
-    // let json = JSON.stringify(data, null, 2)
-    // fs.writeFile('./logs/quotes.json', json, (err) => {
-    //     if (err) console.log('Error ocurred while saving to json')
-    //     console.log(`${counter} products saved to products.json`)
-    // })
-}).catch(err => {
-    console.error('Error after executing multiple crawlers', err)
+// function used to simply insert new data to the db
+// executeMultipleCrawlers().then(data => {
+//     let counter = 0
+//     data.forEach(element => {
+//         counter += 1
+//     })
+//     console.log(`${counter} products scraped`)
+//     db.insertManyQuotes(data)
+//     .then(docs => {
+//         console.log(`Batch succesfull. ${docs.length} quotes saved to MongoDB`)
+//     })
+//     .catch(err => {
+//         console.log('Error executing batch')
+//     })
+//     // let json = JSON.stringify(data, null, 2)
+//     // fs.writeFile('./logs/quotes.json', json, (err) => {
+//     //     if (err) console.log('Error ocurred while saving to json')
+//     //     console.log(`${counter} products saved to products.json`)
+//     // })
+// }).catch(err => {
+//     console.error('Error after executing multiple crawlers', err)
+// })
+
+// function used to update and insert new data to the db
+loadJson().then(data => {
+    console.log(data.length)
+
+    return db.updateBatchQuotes(data)
+})
+.then(results => {
+    console.log('update batch success!!')
+    console.log(results)
+})
+.catch(err => {
+    console.log(err)
 })
 
 function executeMultipleCrawlers () {
@@ -48,6 +63,16 @@ function executeMultipleCrawlers () {
         .catch(err => {
             console.log('Error from sequentially executing the crawlers: ', err)
             reject(err)
+        })
+    })
+}
+
+function loadJson () {
+    return new Promise ((resolve, reject) => {
+        fs.readFile('logs/quotes_update.json', (err, data) => {
+            if (err) reject(err)
+            const quotesArray = JSON.parse(data)
+            resolve(quotesArray)
         })
     })
 }
